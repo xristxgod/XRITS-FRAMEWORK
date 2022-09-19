efc0467b3ca971363736070c7e87980b037dd23f
Mamedov Murad <mamedov_99b@mail.ru>
2022-09-19 13:17:57 +0300

    [ADD] sql builder

--- src/Xrist/orm/_query.py
+++ src/Xrist/orm/_query.py
@@ -1,25 +1,119 @@
+from typing import Any, Optional, Generator
+
+
+AND = "AND"
+OR = "OR"
+
+
+class Q:
+
+    def __init__(self, exp_type: str = AND, **kwargs):
+        self.separator = exp_type
+        self._params = kwargs
+
+    def __str__(self):
+        return f" {self.separator} ".join([f"{key} = {value}" for key, value in self._params.items()])
+
+    def __bool__(self):
+        return bool(self._params)
+
+
+class BaseExp:
+    name = None
+
+    def add(self, *args, **kwargs):
+        raise NotImplementedError()
+
+    def definition(self) -> str:
+        return self.name + "\n\t" + self.line() + '\n'
+
+    def line(self) -> str:
+        raise NotImplementedError()
+
+    def __bool__(self):
+        raise NotImplementedError()
+
+
+class _Select(BaseExp):
+    name = "SELECT"
+
+    def __init__(self):
+        self._params = []
+
+    def add(self, *args, **kwargs) -> Optional:
+        self._params.extend(args)
+
+    def line(self) -> str:
+        separator = ","
+        return separator.join(self._params)
+
+    def __bool__(self):
+        return bool(self._params)
+
+
+class _From(BaseExp):
+    name = "FROM"
+
+    def __init__(self):
+        self._params = []
+
+    def add(self, *args, **kwargs) -> Optional:
+        self._params.extend(args)
+
+    def line(self) -> str:
+        separator = ","
+        return separator.join(self._params)
+
+    def __bool__(self):
+        return bool(self._params)
+
+
+class _Where(BaseExp):
+    name = "WHERE"
+
+    def __init__(self, exp_type: str = AND, **kwargs):
+        self._q = Q(exp_type=exp_type, **kwargs)
+
+    def add(self, exp_type: str = AND, **kwargs) -> 'Q':
+        self._q = Q(exp_type, **kwargs)
+        return self._q
+
+    def line(self) -> str:
+        return str(self._q)
+
+    def __bool__(self):
+        return bool(self._q)
+
+
 class Query:
     def __init__(self):
         self._data = {
-            "select": [],
-            "from": []
+            "select": _Select(),
+            "from": _From(),
+            "where": _Where()
         }
 
-    def SELECT(self, *args):
-        self._data["select"].extend(args)
+    def SELECT(self, *args: str) -> 'Query':
+        self._data["select"].add(*args)
+        return self
 
-    def FROM(self, *args):
-        self._data["from"].extend(args)
+    def FROM(self, *args: str) -> 'Query':
+        self._data["from"].add(*args)
+        return self
 
-    def _line(self, key):
-        separator = ","
-        return separator.join(self._data[key])
+    def WHERE(self, exp_type: str = AND, **kwargs) -> 'Query':
+        self._data["where"].add(exp_type, **kwargs)
+        return self
 
-    def _lines(self):
-        for key in self._data.keys():
-            yield key.upper() + '\n'
-            yield '\t' + self._line(key) + '\n'
+    def _lines(self) -> Generator:
+        for key, value in self._data.items():
+            yield value.definition()
 
     def __str__(self):
         return "".join(self._lines())
+
+
+__all__ = [
+    "Query"
+]
 
